03b15dfcab9c658229eb5f5d99e8bb38a6fcacdd
DawidKurkiewicz <dawidkurkiewicz516@gmail.com>
2019-01-22 16:13:43 +0100

    class game

--- script.js
+++ script.js
@@ -74,11 +74,37 @@ const draw = new Draw()
 
 class Result {
     static moneyWinInGame(result, bid) {
-        if(result) return 3 * bid;
+        if (result) return 3 * bid;
         else return 0
     }
     static checkWinner(draw) {
-        if(draw[0] === draw[1] && draw [1] === draw [2] || draw[0] !== draw[1] && draw[1] !== draw[2] && draw[0] !== draw[2] ) return true;
-        else return false 
+        if (draw[0] === draw[1] && draw[1] === draw[2] || draw[0] !== draw[1] && draw[1] !== draw[2] && draw[0] !== draw[2]) return true;
+        else return false
+    }
+}
+
+class Game {
+    constructor() {
+        this.start = new Statistics()
+        this.wallet = new Wallet(100)
+        document.getElementById("start").addEventListener("click", this.startGame)
+        this.spanWallet = document.querySelector(".panel span.wallet")
+        this.boards = [...document.querySelectorAll('div.color')];
+        this.inputBid = document.getElementById('bid');
+        this.spanResult = document.querySelector('.score span.result');
+        this.spanGames = document.querySelector('.score span.number');
+        this.spanWins = document.querySelector('.score span.win');
+        this.spanLosses = document.querySelector('.score span.loss');
+
+        this.render()
+
+    }
+
+    render() {
+
+    }
+
+    startGame() {
+
     }
 }
