39baca7e58eca5282a47681b14b6ccd0b266be70
DawidKurkiewicz <dawidkurkiewicz516@gmail.com>
2019-01-22 17:15:35 +0100

    make files for all classes

---
+++ Draw.js
@@ -0,0 +1,21 @@
+
+class Draw {
+    constructor() {
+        this.options = ["red", "green", "blue"];
+        let _result = this.drawResults()
+        this.getDrawResult = () => _result
+    }
+
+    drawResults() {
+        let colors = [];
+        for (let i = 0; i < this.options.length; i++) {
+            const index = Math.floor(Math.random() * this.options.length)
+            const color = this.options[index]
+            colors.push(color)
+        }
+        return colors
+    }
+
+}
+
+const draw = new Draw()
